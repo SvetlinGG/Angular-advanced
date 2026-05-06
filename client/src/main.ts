abec1da40cd75b2a1bd4868146ac90aba3a55eed
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-05-06 11:27:05 +0200

    create data file

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,6 +1,13 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http';
+import { importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideHttpClient(),
+    provideZoneChangeDetection(),importProvidersFrom(BrowserModule),
+  ]
+})
   .catch((err) => console.error(err));
