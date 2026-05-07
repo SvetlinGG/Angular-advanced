74458ce41b2e6f5fe6837e173196c21e7dab5520
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-05-07 21:38:09 +0200

    course-image  HTML/CSS

--- client/src/app/courses/course-image/course-image.component.ts
+++ client/src/app/courses/course-image/course-image.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-course-image',
+  standalone: true,
   imports: [],
   templateUrl: './course-image.component.html',
   styleUrl: './course-image.component.css'
 })
-export class CourseImageComponent {
+export class CourseImageComponent implements OnInit {
+
+  @Input('src')
+  imageUrl: string;
+
+
+  constructor(){}
+
+
+  ngOnInit(): void {
+    
+  }
 
 }
